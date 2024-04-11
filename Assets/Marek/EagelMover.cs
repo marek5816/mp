6ca93db9cff4eae671db639aaae5b472e4cc37e2
Norbert Takáč <fasd@fdjask.sk>
2024-04-11 11:48:54 +0200

    Eagel dieDespawn function

--- Assets/Marek/EagelMover.cs
+++ Assets/Marek/EagelMover.cs
@@ -8,21 +8,58 @@ public class EagelMover : MonoBehaviour
     public Vector3 direction = Vector3.right;
     public float distanceToTravel = 50f;
     private Vector3 startPosition;
+    
+    private Animator animator;
+    private bool isDying = false;
+    private bool dyingSwitch = true;
 
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        if (isDying == true) {
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Death") ||
+                animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                Destroy(gameObject);
+            }
 
-        if (Vector3.Distance(startPosition, transform.position) >= distanceToTravel)
-        {
-            Destroy(gameObject);
+            /*if (dyingSwitch) {
+                dyingSwitch = false;
+                animator.Play("Death");
+            }*/
+        } else {
+            dieDespawn();
+            transform.Translate(direction * speed * Time.deltaTime);
+
+            if (Vector3.Distance(startPosition, transform.position) >= distanceToTravel)
+            {   
+                Destroy(gameObject);
+
+                
+    /*
+                animator.Play("Eagle_Die");
+                isDying = true;
+                if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Eagle_Die") ||
+                    animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                {
+                    //Destroy(gameObject);
+                }
+                //Destroy(gameObject);
+    */
+            }
         }
+
+    }
+
+    public void dieDespawn() {
+        isDying = true;
+        animator.Play("Death");
     }
 }
